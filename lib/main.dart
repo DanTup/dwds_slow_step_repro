2eff437e9c40aa6eb674a2ede1fc966e2c45d351
Danny Tuppeny <danny@tuppeny.com>
2019-10-30 16:40:48 +0000

    Repro

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var m = true;
+    if (m) {
+      print("Hello, world!");
+    }
+
+    return Text(
+      'Hello, world!',
+      textDirection: TextDirection.ltr,
+    );
+  }
+}
